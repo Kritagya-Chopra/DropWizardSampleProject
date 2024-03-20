0444b6eb94eeb2e921c0f606f364223032e0d356
Kritagya Chopra <er.kritagyachopra@gmail.com>
2024-03-20 17:12:16 +0530

    Added Skeleton for Unit Tests

---
+++ ProductMicroservice/src/test/java/org/example/api/ProductsServiceTest.java
@@ -0,0 +1,114 @@
+package org.example.api;
+
+import org.junit.jupiter.api.Test;
+
+public class ProductsServiceTest {
+
+    @Test
+    public void testGetProducts_ReturnsListOfProducts() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testGetProducts_WithIds_ReturnsListOfProducts() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testGetProducts_WithNonExistingIds_ThrowsWebApplicationException() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testCreateProduct_CreatesProductSuccessfully() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testEditProduct_EditsProductSuccessfully() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testEditProduct_WithNonExistingProduct_ThrowsWebApplicationException() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testDeleteProduct_DeletesProductSuccessfully() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testDeleteProduct_WithNonExistingProduct_ThrowsWebApplicationException() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testDeleteProduct_WithUnexpectedError_ThrowsWebApplicationException() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testPlaceOrder_UpdatesQuantitySuccessfully() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testPlaceOrder_WithInsufficientStock_ThrowsWebApplicationException() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testPlaceOrder_WithNonExistingProduct_ThrowsWebApplicationException() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
